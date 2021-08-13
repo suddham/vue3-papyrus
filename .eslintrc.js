9cccf44d6c29296a947dda44ad1ad3953d298ebb
Suddha M <suddha@banxa.com>
2021-08-13 20:18:21 +0530

    Init with base configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,23 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 4],
+    'no-tabs': 0,
+    'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+    'comma-dangle': ['error', 'never'],
+    'import/extensions': 'off',
+    'max-len': ['off', {
+      code: 100,
+      ignoreUrls: true
+    }]
   },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src']
+        ]
+      }
+    }
+  }
 };
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <navigation />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld';
+import Navigation from './components/Navigation';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+    name: 'App',
+    components: {
+        Navigation,
+        HelloWorld
+    }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
